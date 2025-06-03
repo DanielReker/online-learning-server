fb0ce4c3ee25c2e5c3ad497a68fc2cf09172d064
DanielReker <sasha29072004@gmail.com>
2025-06-04 00:48:40 +0400

    Fix package name

--- src/main/java/io/github/danielreker/onlinelearningserver/OnlineLearningServerApplication.java
+++ src/main/java/io/github/danielreker/onlinelearningserver/OnlineLearningServerApplication.java
@@ -1,4 +1,4 @@
-package io.github.danielreker.assembler.onlinelearningserver;
+package io.github.danielreker.onlinelearningserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
